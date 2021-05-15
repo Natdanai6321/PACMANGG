045c8401a23734dbabc78ca5ad1b98c09a156f9f
Natdanai6321 <54159011+Natdanai6321@users.noreply.github.com>
2021-05-16 02:42:25 +0700

    Finished
    Web tech

---
+++ testProject/script/main.js
@@ -0,0 +1,9 @@
+/* กำหนดฟังชั้นของ Navbar */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
